e8d0ec517c5c52800f368bd8fc760b67f8a7af9c
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2025-01-11 18:39:15 -0500

    :boom: enhance Navbar component with new layout and links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav className="bg-white p-3 shadow-lg">TODO: NAVBAR</nav>;
+  return (
+    <nav className="bg-white p-6 shadow-lg">
+      <ul className="list-none flex justify-between items-center md:mx-60">
+        <li className="flex space-x-4">
+          <ul>Sahil</ul>
+          <ul>Toggle</ul>
+        </li>
+        <li className="flex space-x-6">
+          <ul>About</ul>
+          <ul>Resume</ul>
+          <ul>Fun</ul>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
