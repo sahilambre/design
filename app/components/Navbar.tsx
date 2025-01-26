b57a28e45b7ae3fedf669b03ba267f7d2257e2fe
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2025-01-26 15:40:14 -0500

    enhance: refactor Navbar component for improved link animations and structure

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,28 +4,37 @@ import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white text-black  p-6 shadow-lg dark:shadow-white-lg dark:bg-black dark:text-white ">
-      {" "}
-      {/* //dark:bg-black dark:text-white */}
+    <nav className="bg-white text-black p-6 shadow-lg dark:shadow-white-lg dark:bg-black dark:text-white">
       <ul className="list-none flex justify-between items-center md:mx-60">
+        {/* Left Section */}
         <li className="flex space-x-4">
           <ul className="text-xl md:text-3xl font-[poppins]">
             <Link href="/">Sahil.</Link>
           </ul>
           <ul>
-            <ThemeToggle />{" "}
+            <ThemeToggle />
           </ul>
         </li>
+
+        {/* Right Section */}
         <li className="flex space-x-6">
-          <ul className="font-[poppins]">
-            <Link href="/about">About</Link>
-          </ul>
-          <ul className="font-[poppins]">
-            <Link href="/resume">Resume</Link>
-          </ul>
-          <ul className="font-[poppins]">
-            <Link href="/fun">Fun</Link>
-          </ul>
+          {["About", "Resume", "Fun"].map((text) => (
+            <ul className="font-[poppins]" key={text}>
+              <div className="relative overflow-hidden group">
+                <span className="invisible">{text}</span>
+                <span className="absolute top-0 left-0 group-hover:-translate-y-full transition-transform ease-in-out duration-500 hover:duration-300">
+                  {text}
+                </span>
+                <span className="absolute top-0 left-0 translate-y-full group-hover:translate-y-0 transition-transform ease-in-out duration-500 hover:duration-300">
+                  {text}
+                </span>
+                <Link
+                  href={`/${text.toLowerCase()}`}
+                  className="absolute inset-0"
+                ></Link>
+              </div>
+            </ul>
+          ))}
         </li>
       </ul>
     </nav>
