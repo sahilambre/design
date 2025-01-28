720588012d516b5668cdf5671a78f612a68b3509
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2025-01-27 23:48:00 -0500

    enhance: update About component with improved styling and content

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import aboutImage from "@/public/iwritecode.webp";
+import { motion } from "motion/react";
 
 const About = () => {
   return (
     <>
       <div className="flex flex-col lg:flex-row h-[80vh]">
         {/* Image Section */}
-        <div className="w-full lg:w-1/2 bg-gray-400 flex justify-center items-center p-4">
+        <div className="w-full lg:w-1/2 bg-gray-400 dark:bg-black dark:text-white flex justify-center items-center p-4">
           <Image
             src={aboutImage}
             alt="aboutImage"
@@ -19,12 +20,15 @@ const About = () => {
         </div>
 
         {/* Text Section */}
-        <div className="w-full lg:w-1/2 flex justify-center items-center p-4 border border-solid-black">
-          <p className="text-center text-2xl px-12 font-medium">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            euismod nisi a risus venenatis, et efficitur metus vehicula. Sed ac
-            ligula et ipsum fermentum hendrerit. Duis tristique dui sit amet
-            tincidunt volutpat.
+        <div className="w-full lg:w-1/2 flex justify-center items-center p-4 border border-solid-black dark:bg-black dark:text-white">
+          <p className="text-center text-3xl px-12 font-medium font-[source-serif-pro]">
+            Welcome to my corner of the web! <br />
+            <span className="font-[source-serif-pro] text-2xl">
+              I'm a curious individual passionate about exploring technology,
+              trends, and intriguing conversations. This site reflects my
+              journey, showcasing insights and ideas. I'm excited to share my
+              thoughts and connect with others who share similar interests.
+            </span>
           </p>
         </div>
       </div>
